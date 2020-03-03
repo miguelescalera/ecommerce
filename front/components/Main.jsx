843f3983e5027a35e0b566776b1094fa446a03e3
missvico <57155867+missvico@users.noreply.github.com>
2020-03-03 16:19:30 -0300

    Arreglos start front (#10)

--- front/components/Main.jsx
+++ front/components/Main.jsx
@@ -1,32 +1,33 @@
 import React from 'react'
 import { Route, Redirect, Switch } from "react-router-dom";
-import NavbarContainer from '../containers/NavbarContainer'
-import FooterContainer from '../containers/FooterContainer'
+// import NavbarContainer from '../containers/NavbarContainer'
+// import FooterContainer from '../containers/FooterContainer'
 import PaginaPrincipalContainer from '../containers/PaginaPrincipalContainer'
-import ProductsContainer from '../containers/ProductsContainer'
-import ProductContainer from '../containers/ProductContainer'
-import RegisterContainer from '../containers/RegisterContainer'
-import CarritoContainer from '../containers/CarritoContainer'
-import CheckoutContainer from '../containers/CheckoutContainer'
-import UserContainer from '../containers/UserContainer'
+// import ProductsContainer from '../containers/ProductsContainer'
+// import ProductContainer from '../containers/ProductContainer'
+// import RegisterContainer from '../containers/RegisterContainer'
+// import CarritoContainer from '../containers/CarritoContainer'
+// import CheckoutContainer from '../containers/CheckoutContainer'
+// import UserContainer from '../containers/UserContainer'
 
 export default ()=>{
 
     return(
         <div id='main'>
-            <NavbarContainer/>
+            <p> Hola React</p>
+            {/* <NavbarContainer/> */}
             <Switch>
             <Route path="/home" exact component={PaginaPrincipalContainer} />
-            <Route path="/products" exact component={ProductsContainer} />
+            {/* <Route path="/products" exact component={ProductsContainer} />
             <Route path="/product/:id" exact component={ProductContainer} />
             <Route path="/register" exact component={RegisterContainer} />
             <Route path="/cart" exact component={CarritoContainer} />
             <Route path="/checkout" exact component={CheckoutContainer} />
-            <Route path="/user/:id" exact component={UserContainer} />
+            <Route path="/user/:id" exact component={UserContainer} /> */}
 
             <Redirect from="/" to="/home" />
             </Switch>
-            <FooterContainer/>
+            {/* <FooterContainer/> */}
 
 
         </div>
