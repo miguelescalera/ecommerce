1523dbe90c388a92714f5ed3df6ea98dabd98666
Matiaspotti <matias.pugliese@hotmail.com>
2020-03-03 14:46:06 -0300

    Componentes y containers (#7)
    * componentescreados
    
    * hola
    
    * helloworld
    
    Co-authored-by: jodch <56612511+jodch@users.noreply.github.com>

---
+++ front/containers/PaginaPrincipalContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PaginaPrincipal from '../components/PaginaPrincipal'
+
+export default class PaginaPrincipalContainer extends React.Component{
+    render(){
+        return(
+            <div>
+                <PaginaPrincipal/>
+            </div>
+        )
+    }
+}
