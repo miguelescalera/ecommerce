b13c59328bda6b2315eceabbcc32a6582e91b82a
fernandezAlan <alan.fer09@gmail.com>
2020-03-03 11:09:19 -0300

    passport y server termiando

---
+++ back/app.js
@@ -0,0 +1,74 @@
+const express = require("express")
+const app = express()
+
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+const sequelize = require("./models/index")
+const Routers= require("./routes/index")
+const productsRouters= Routers.Products
+const UsersRouters = Routers.Users
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
+
+app.use(session({ 
+    secret: "bootcamp",
+    resave: true,
+    saveUninitialized: true
+  }))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new LocalStrategy({ inputEmail: 'email' },
+  function(inputEmail, password, done) {
+    
+    User.findOne({ where: {email: inputEmail} })
+      .then(user => {
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user); 
+      })
+      .catch(done);
+  }
+));
+
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+
+passport.deserializeUser(function(id, done) {
+    User.findByPk(id)
+        .then(user => done(null, user))
+});
+
+function isLogedIn(req, res, next) {
+  if (req.isAuthenticated()) {       
+    next();
+  } else {
+    res.redirect('/');
+  }
+}
+
+app.use("/products", productsRouters)
+app.use("/users", UsersRouters)
+
+
+sequelize.sync().then(function(){
+    console.log("database ready")
+        app.listen("3000",function(){
+            console.log("server on port 3000")
+        })
+    })
+    
