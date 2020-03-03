f2d2e0b2b93178a0f91e8335343bd4099a9cc239
jodch <josedavidchiquillo@gmail.com>
2020-03-03 14:28:17 -0300

    hola

---
+++ front/components/Main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Route, Redirect, Switch } from "react-router-dom";
+import NavbarContainer from '../containers/NavbarContainer'
+import FooterContainer from '../containers/FooterContainer'
+import PaginaPrincipalContainer from '../containers/PaginaPrincipalContainer'
+import ProductsContainer from '../containers/ProductsContainer'
+import ProductContainer from '../containers/ProductContainer'
+import RegisterContainer from '../containers/RegisterContainer'
+import CarritoContainer from '../containers/CarritoContainer'
+import CheckoutContainer from '../containers/CheckoutContainer'
+import UserContainer from '../containers/UserContainer'
+
+export default ()=>{
+
+    return(
+        <div id='main'>
+            <NavbarContainer/>
+            <Switch>
+            <Route path="/home" exact component={PaginaPrincipalContainer} />
+            <Route path="/products" exact component={ProductsContainer} />
+            <Route path="/product/:id" exact component={ProductContainer} />
+            <Route path="/register" exact component={RegisterContainer} />
+            <Route path="/cart" exact component={CarritoContainer} />
+            <Route path="/checkout" exact component={CheckoutContainer} />
+            <Route path="/user/:id" exact component={UserContainer} />
+
+            <Redirect from="/" to="/home" />
+            </Switch>
+            <FooterContainer/>
+
+
+        </div>
+
+    )
+}
+
