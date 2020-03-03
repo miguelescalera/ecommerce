004d290e1063e1a63972ee70707c452f67aa73a9
jodch <josedavidchiquillo@gmail.com>
2020-03-03 14:44:33 -0300

    helloworld

---
+++ front/components/PaginaPrincipal.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Jumbotron from 'react-bootstrap/Alert'
+
+export default ({ }) => {
+    return (
+        <div>
+            <Jumbotron>
+                <h1>Hello, world!</h1>
+                <p>
+                    Aqui va a estar nuestro ecommerce wineNot por que no? por que si.
+                </p>
+                <p>
+                    <Button variant="primary">No Hago Nada</Button>
+                </p>
+            </Jumbotron>
+        </div>
+    )
+}
