7f1a331ba6096fadfc209f954b0aa1c1b625acb0
jodch <josedavidchiquillo@gmail.com>
2020-03-03 14:25:10 -0300

    componentescreados

---
+++ front/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Main from "./components/Main";
+import { Provider } from "react-redux";
+import store from './redux/store'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/" component={Main} />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("app")
+);
