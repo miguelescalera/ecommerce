c488a52bc77f09e694c58a97ab20e59cae37ba50
Victoria Di Liscia <victoria@10.100.0.104>
2020-03-03 16:11:41 -0300

    Arreglos start front

--- front/components/PaginaPrincipal.jsx
+++ front/components/PaginaPrincipal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Jumbotron from 'react-bootstrap/Alert'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
 
 export default ({ }) => {
     return (
