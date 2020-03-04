f681fe358dfc687d65687b78cb65629875780197
fernandezAlan <59181648+fernandezAlan@users.noreply.github.com>
2020-03-04 15:11:11 -0300

    arreglada la grilla (#25)

--- front/components/TarjetaProducto.jsx
+++ front/components/TarjetaProducto.jsx
@@ -1,29 +1,17 @@
 import React from "react"
-import Card from 'react-bootstrap/Card'
 
-const TarjetaProducto=function({foundProducts}){
-   let products = foundProducts.map(function(prd){
-                <div key={prd.id} >
-                    <Card>
-                        <Link to ={`/product/${prd.id}`}>
-                            <Card.Img variant="top" src={prd.imgUrl} />
-                            <Card.Body>
-                                <Card.Title>{prd.name}</Card.Title>
-                                <Card.Text>
-                                    {prd.description}
-                                </Card.Text>
-                            </Card.Body>
-                        </Link>
-                    </Card>
-                </div>
-        })
+
+const TarjetaProducto=function(/*{foundProducts}*/){
+
+  
+
+
+   
                         
 
    
     return(
-           <div>
-               {products}
-           </div> 
+          
     )
 }
 
--- front/containers/ProductsContainer.jsx
+++ front/containers/ProductsContainer.jsx
@@ -1,18 +1,68 @@
 import React from "react"
 import { connect } from "react-redux"
-import TarjetaProducto from "../components/TarjetaProducto"
+import Card from 'react-bootstrap/Card'
+import {Link} from "react-router-dom"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-class ProductConatainer extends React.Component{
+//ESTE ES INFORMACION DE PRUEBA TODO ESTO DEBERIA VENIR DEL STORE
+const foundProducts = [
+    {name:"este es un vinito",description:"alto vinito, super rico y barato", imgUrl:"https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3060542_f.jpg"},
+    {name:"este es un vinito",description:"alto vinito, super rico y barato", imgUrl:"https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3060542_f.jpg"},
+    {name:"este es un vinito",description:"alto vinito, super rico y barato", imgUrl:"https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3060542_f.jpg"},
+    {name:"este es un vinito",description:"alto vinito, super rico y barato", imgUrl:"https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3060542_f.jpg"},
+    {name:"este es un vinito",description:"alto vinito, super rico y barato", imgUrl:"https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3060542_f.jpg"},
+    {name:"este es un vinito",description:"alto vinito, super rico y barato", imgUrl:"https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3060542_f.jpg"},
+    {name:"este es un vinito",description:"alto vinito, super rico y barato", imgUrl:"https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3060542_f.jpg"},
+    {name:"este es un vinito",description:"alto vinito, super rico y barato", imgUrl:"https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3060542_f.jpg"},
+]
+////////////////////////////////////////////////////////////////////////////////
+let products = foundProducts.map(function(prd){
+    return(  
+                <Col sm={4}>
+                    <div key={prd.id} >
+                        <Card>
+                            <Link to ={`/product/${prd.id}`}>
+                                <Card.Img variant="top" src={prd.imgUrl} />
+                                <Card.Body>
+                                    <Card.Title>{prd.name}</Card.Title>
+                                    <Card.Text>
+                                        {prd.description}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Link>
+                        </Card>
+                    </div>
+                </Col>
+
+           )
+       })
+           
+
+
+
+class ProductsContainer extends React.Component{
 constructor(){
     super()
 }
         render(){
+            
+            return(
+
             <div>
-                <h3>resultado de la busqueda</h3>
-                <TarjetaProducto foundProducts={this.props.foundProducts}/>
+                
+                <Container>
+                <Row>
+                   {products}
+                </Row>
+                </Container>
             </div>
+                )
+
+            }
         }
-}
+
 
 const mapStateToProps = function (state) {
     return {
@@ -20,4 +70,4 @@ const mapStateToProps = function (state) {
         
     };
 }
-export default connect(mapStateToProps)(ProductConatainer);
+export default connect(mapStateToProps)(ProductsContainer);
