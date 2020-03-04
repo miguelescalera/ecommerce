6e9634d1683d501738b992c133c51089534f008e
missvico <57155867+missvico@users.noreply.github.com>
2020-03-04 15:22:35 -0300

    Single product final (#24)
    * terminado single product
    
    * terminado
    
    * terminado posta
    
    Co-authored-by: Matiaspotti <matias.pugliese@hotmail.com>

---
+++ front/components/Product.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default ({ selectedProduct }) => {
+  console.log(selectedProduct);
+  return (
+    <div key={selectedProduct.id}>
+      <h1>{selectedProduct.name}</h1>
+      <img src={selectedProduct.imgUrl} alt="" />
+      <p>{selectedProduct.description}</p>
+      <p>${selectedProduct.price}</p>
+    </div>
+  );
+};
--- front/reducers/index.js
+++ front/reducers/index.js
@@ -1,9 +1,8 @@
-import { combineReducers } from 'redux';
-import foundProduct from "./searchProductsReducer"
+import { combineReducers } from "redux";
+import foundProduct from "./searchProductsReducer";
+import selectedProduct from "./selectProduct";
 
-export default combineReducers(
-    { 
-        foundProduct:foundProduct
-    
-    }
- )
+export default combineReducers({
+  foundProduct: foundProduct,
+  selectedProduct: selectedProduct
+});
