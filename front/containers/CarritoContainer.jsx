78f42d64b8e11d57c74f8cc02a829d554ceb8bfd
miguelescalera <58999057+miguelescalera@users.noreply.github.com>
2020-03-05 16:18:48 -0300

    Resumen compra (#42)
    * agregue el history para redirigir
    
    * resumen de compra

---
+++ front/containers/CarritoContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Carrito from '../components/Carrito'
+
+export default class CarritoContainer extends React.Component{
+    constructor(){
+        super()
+    }
+
+render(){
+    return(
+        <div>
+            <Carrito/>
+        </div>
+    )
+}
+}
+
+
