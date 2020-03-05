3e20a8cd26aa0c009fd876edec5e8945bf23b16d
Matiaspotti <matias.pugliese@hotmail.com>
2020-03-05 10:23:08 -0300

    bugfixSearch

--- back/db/index.js
+++ back/db/index.js
@@ -1,8 +1,11 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://localhost:5432/winenot", {
-  logging: false,
-  dialect: "postgres"
-});
+const db = new Sequelize(
+  "postgres://postgres:matute10@localhost:5432/winenot",
+  {
+    logging: false,
+    dialect: "postgres"
+  }
+);
 
 module.exports = db;
