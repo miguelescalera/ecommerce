ec7aac8a71897ebfe2160d3d6a8811a6299c395f
Victoria Di Liscia <victoria@Air-de-Victoria.utopia.net>
2020-03-11 23:05:26 -0300

    Para mergear me faltaron cosas del front

--- front/components/OrdenesPrivate.jsx
+++ front/components/OrdenesPrivate.jsx
@@ -1,99 +1,10 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-=======
 import React from "react";
->>>>>>> 41fd912906d3143facc778db3e91b50bfe77db55
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import Accordion from "react-bootstrap/Accordion";
 import Button from "react-bootstrap/Button";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from 'react-bootstrap/Dropdown'
-<<<<<<< HEAD
-import Collapse from 'react-bootstrap/Collapse'
-
-const OrdenesPrivate = function ({ ordenes }) {
-
-  const [open, setOpen] = useState(false);
-
-  const tablaStyles = {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    width: '850px',
-  }
-
-  const padding = {
-    paddingBlockStart:'1rem',
-    paddingTopStart:'1rem',
-    boxShadow :'8px 8px 15px -10px rgba(0,0,0,0.39)',
-
-  }
-
-
-  return (
-    <div style={padding}>
-
-    <Card key={ordenes.id} style={tablaStyles}>
-      <Table hover
-        onClick={() => setOpen(!open)}
-        aria-controls={ordenes.id}
-        aria-expanded={open}
-      >
-        <tbody>
-          <tr>
-            <td>{ordenes.id}</td>
-            <td> {ordenes.User.firstName + " " + ordenes.User.lastName}</td>
-            <td>{"$" + ordenes.subTotal}</td>
-            <td>{ordenes.createdAt}</td>
-            <td>{ordenes.updatedAt}</td>
-            <td>
-              <DropdownButton
-                style={{ width: '100px' }}
-                id="dropdown-item-button"
-                title={ordenes.status}
-
-              >
-                <Dropdown.Item style={{ width: '100px' }} as="button" variant='dark' >Process</Dropdown.Item>
-                <Dropdown.Item style={{ width: '100px' }} as="button" variant='dark' >Complete</Dropdown.Item>
-              </DropdownButton>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-
-      <Collapse in={open}>
-        <Card.Body id={ordenes.id}>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Producto</th>
-                <th>Cantidad</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {ordenes.products.map(producto => {
-                return (
-                  <tr>
-                    <td>{producto.name}</td>
-                    <td>{producto.order_products.quantity}</td>
-                    <td>{"$" + producto.order_products.totalPrice}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        </Card.Body>
-      </Collapse>
-    </Card>
-    </div>
-  );
-};
-export default OrdenesPrivate;
-      
-
-=======
 
 const OrdenesPrivate = function({ ordenes }) {
   return (
@@ -142,4 +53,3 @@ const OrdenesPrivate = function({ ordenes }) {
   );
 };
 export default OrdenesPrivate;
->>>>>>> 41fd912906d3143facc778db3e91b50bfe77db55
